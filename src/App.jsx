00526220521887d0aed367462ae33524017b5c67
alex Kochens <ofwonoalex256@gmail.com>
2026-02-10 13:54:13 +0300

    feat: add grid layout to coins

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,9 @@ const App = () => {
           alt='crypto-dashboard-logo'
           className='w-10 h-10  rounded-lg'
         />
-        <h1 className='text-black-900 font-bold text-2xl '>Crypto Dashboard</h1>
+        <h1 className='text-black-900 font-bold text-2xl  '>
+          Crypto Dashboard
+        </h1>
       </div>
 
       {loading && <p>Loading...</p>}
@@ -45,25 +47,29 @@ const App = () => {
         </p>
       )}
 
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 mt-5'>
         {coins.map((coin) => (
-          <div key={coin.id} className='p-6'>
-            <h1 className=' font-medium text-1xl'>{coin.name}</h1>
-            <img
-              src={coin.image}
-              alt={coin.name}
-              className='w-15 h-15 rounded-lg'
-            />
-
-            <p className='text-neutral-600'>{coin.symbol.toUpperCase()}</p>
-            <p>{coin.current_price.toLocaleString()}</p>
-            <p>Market_Cap: {coin.market_cap.toLocaleString()}</p>
+          <div
+            key={coin.id}
+            className='p-3 bg-white rounded-lg  border border-gray-200 flex flex-col items-center text-center'
+          >
+            <img src={coin.image} alt={coin.name} className='w-16 h-16 mb-2' />
+            <h1 className='font-semibold text-lg'>{coin.name}</h1>
+            <p className='text-gray-500 uppercase'>{coin.symbol}</p>
+            <p className='font-medium mt-1'>
+              Price: ${coin.current_price.toLocaleString()}
+            </p>
+            <p className='text-gray-500'>
+              Market_Cap: ${coin.market_cap.toLocaleString()}
+            </p>
             <p
               className={
-                coin.price_change_24h >= 0 ? 'text-green-400' : 'text-red-400'
+                coin.price_change_24h >= 0
+                  ? 'text-green-500 font-semibold'
+                  : 'text-red-500 font-semibold'
               }
             >
-              {coin.price_change_24h.toLocaleString()}
+              {coin.price_change_24h.toFixed(2)}%
             </p>
           </div>
         ))}
